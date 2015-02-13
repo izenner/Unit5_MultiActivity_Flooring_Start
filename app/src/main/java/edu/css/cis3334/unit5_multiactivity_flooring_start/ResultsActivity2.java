c4f2090b8691a999b9d6ce3d789be83ea5265c2a
izenner <izenner@css.edu>
2015-02-13 13:41:38 -0600

    Added new activity

--- app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/ResultsActivity2.java
+++ app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/ResultsActivity2.java
@@ -1,32 +1,24 @@
 package edu.css.cis3334.unit5_multiactivity_flooring_start;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ResultsActivity2 extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_results_activity2);
     }
 
-
-    public void onClick(View v) {
-        // declare an intent for our new activity
-        Intent i = new Intent(this, ResultsActivity2.class);
-        startActivity(i);       // display new activity
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_results_activity2, menu);
         return true;
     }
 
